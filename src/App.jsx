c7343954a9e2903c1984fb8da8ebc0470ee9cf74
Phodi <5670253+Phodi@users.noreply.github.com>
2020-04-02 22:25:44 +0700

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
+import About from "./components/ui/About"
 
 function App() {
-  return <div>Hello World</div>
+  return (
+    <div>
+      <About />>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/ui/About.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Developer information</h5>
+            <p>Phodi Phoocharoen (610610601)</p>
+            <p>This app use Google Firebase as backend.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
