144a0406284c14b173e1ac3318f79430b0372fb8
Phodi <5670253+Phodi@users.noreply.github.com>
2020-04-04 20:28:36 +0700

    fix about className

--- src/components/homepage/subcomponents/NavMenu.jsx
+++ src/components/homepage/subcomponents/NavMenu.jsx
@@ -4,10 +4,7 @@ import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import NavLink from "react-bootstrap/NavLink"
 import { Link } from "react-router-dom/"
-
-import Login from "../../Login"
-
-class NavbarMenu extends Component {
+class NavMenu extends Component {
   render() {
     return (
       <Navbar sticky="top">
@@ -32,53 +29,6 @@ class NavbarMenu extends Component {
       </Navbar>
     )
   }
-  // render() {
-  //   return (
-  //     <section id="nav-bar">
-  //       {" "}
-  //       <nav class="navbar navbar-expand-lg navbar-light">
-  //         <a class="navbar-brand" href="#">
-  //           <img src="img/logo.jpg"></img>
-  //         </a>
-  //         <button
-  //           class="navbar-toggler"
-  //           type="button"
-  //           data-toggle="collapse"
-  //           data-target="#navbarNav"
-  //           aria-controls="navbarNav"
-  //           aria-expanded="false"
-  //           aria-label="Toggle navigation"
-  //         >
-  //           <span class="navbar-toggler-icon"></span>
-  //         </button>
-  //         <div class="collapse navbar-collapse" id="navbarNav">
-  //           <ul class="navbar-nav ml-auto">
-  //             <li class="nav-item">
-  //               <a class="nav-link" href="#">
-  //                 HOME
-  //               </a>
-  //             </li>
-  //             <li class="nav-item">
-  //               <a class="nav-link" href="#">
-  //                 PLAY
-  //               </a>
-  //             </li>
-  //             <li class="nav-item">
-  //               <a class="nav-link" href="#">
-  //                 SCOREBOARD
-  //               </a>
-  //             </li>
-  //             <li class="nav-item">
-  //               <a class="nav-link" href="#">
-  //                 OUR TEAM
-  //               </a>
-  //             </li>
-  //           </ul>
-  //         </div>
-  //       </nav>
-  //     </section>
-  //   )
-  // }
 }
 
-export default NavbarMenu
+export default NavMenu
