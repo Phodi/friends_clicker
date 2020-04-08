6cc14b02d762d0df9acf922563a78885821a580f
46592405 <nitiwit2011@gmail.com>
2020-04-08 13:47:28 +0700

    removeAccordion

--- src/components/about/component/Aboutus.jsx
+++ src/components/about/component/Aboutus.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 import Accordion from 'react-bootstrap/Accordion'
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-class Sus extends Component{
+class Aboutus extends Component{
     constructor(props) {
         super(props)
         this.state = {}
@@ -12,56 +12,54 @@ class Sus extends Component{
         <div>
           <center>
           
-            
             <div class ="container">
               <div class="row">
                 <div class="col-md-4">
                   <div class="list-group-item list-group-item-action" role="tab">
-                  <Accordion>
+                  
                   <Card>
-                  <Accordion.Toggle as={Card.Header} eventKey="0">
+                  
                     
                   <img src="./img/hqdefault.jpg" width="100%" height="auto" alt=""/>
                   
-                  </Accordion.Toggle>
-                  <Accordion.Collapse eventKey="0">
+                  
                   <Card.Body>Hello! I'm the body1</Card.Body>
-                  </Accordion.Collapse>
+           
                   </Card>
-                  </Accordion>
+                  
                   </div>
                 </div>
               <div class="col-md-4">
                 <div class="list-group-item list-group-item-action" role="tab">
                   {/* <img src="./img/hqdefault.jpg" alt=""/> */}
-                  <Accordion>
+                  
                   <Card>
-                  <Accordion.Toggle as={Card.Header} eventKey="0">
+                
                     
                   <img src="./img/hqdefault.jpg" width="100%" height="auto" alt=""/>
                   
-                  </Accordion.Toggle>
-                  <Accordion.Collapse eventKey="0">
+                  
+                  
                   <Card.Body>Hello! I'm the body2</Card.Body>
-                  </Accordion.Collapse>
+                  
                   </Card>
-                  </Accordion>
+                  
                 </div>
               </div>
               <div class="col-md-4">
                 <div class="list-group-item list-group-item-action" role="tab">
-                <Accordion>
+                
                 <Card>
-                  <Accordion.Toggle as={Card.Header} eventKey="0">
+                  
                     
                   <img src="./img/hqdefault.jpg" width="100%" height="auto" alt=""/>
                   
-                  </Accordion.Toggle>
-                  <Accordion.Collapse eventKey="0">
+                  
+                  
                   <Card.Body>Hello! I'm the body3</Card.Body>
-                  </Accordion.Collapse>
+                  
                   </Card>
-                  </Accordion>
+                  
                 </div>
               </div>
             </div>
@@ -75,4 +73,4 @@ class Sus extends Component{
        )
    }
 }
-export default Sus
+export default Aboutus
